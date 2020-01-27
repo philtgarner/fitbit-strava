16c6f9c1d6d4173f90374ef5209d4c04f4bc7238
Philip Garner <philtgarner@gmail.com>
2020-01-27 06:43:09 +0000

    Split login page into two ready for Strava integration.
    Also tidied up sleep graphs

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ ACCESS_TOKEN_KEY = 'access_token'
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-login_url = 'https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=22BCBG&redirect_uri=http%3A%2F%2F127.0.0.1%3A5000%2Fauth&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800'
+login_url = 'https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=22BCBG&redirect_uri=http%3A%2F%2F127.0.0.1%3A5000%2Ffitbitauth&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800'
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.config.suppress_callback_exceptions = True
@@ -32,12 +32,40 @@ app.layout = html.Div([
 ])
 
 def login():
-    return html.Div([
-        html.H3('Log in'),
-        html.A('Log in', href=login_url)
-    ])
+    return dbc.Container(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(
+                        [
+                            html.H3("Fitness dashboard")
+                        ],
+                        md=12,
+                    )
+                ]
+            ),
+            dbc.Row(
+                [
+                    dbc.Col(
+                        [
+                            html.H4("Fitbit"),
+                            html.A('Log in', href=login_url)
+                        ],
+                        md=6,
+                    ),
+                    dbc.Col(
+                        [
+                            html.H4("Strava"),
+                            html.A('Log in', href=login_url)
+                        ],
+                        md=6,
+                    )
+                ]
+            )
+        ]
+    )
 
-def getAccessToken(query):
+def getFitbitAccessToken(query):
 
     if session.get(ACCESS_TOKEN_KEY, None) is None:
         config = yaml.safe_load(open("config.yml"))
@@ -52,7 +80,48 @@ def getAccessToken(query):
 
         # Get an authorisation token
         endpoint = "https://api.fitbit.com/oauth2/token"
-        data = {'clientId': client_id, 'grant_type': 'authorization_code', 'redirect_uri': 'http://127.0.0.1:5000/auth',
+        data = {'clientId': client_id, 'grant_type': 'authorization_code', 'redirect_uri': 'http://127.0.0.1:5000/fitbitauth',
+                'code': code}
+        headers = {"Authorization": f"Basic {auth}",
+                   'Content-Type': 'application/x-www-form-urlencoded'}
+
+        output = json.loads(requests.post(endpoint, data=data, headers=headers).text)
+
+        # A successful call returns the following data:
+        # - access_token
+        # - expires_in
+        # - refresh_token
+        # - scope
+        # - user_id
+        if 'access_token' in output:
+            session[ACCESS_TOKEN_KEY] = output[ACCESS_TOKEN_KEY]
+
+            # Access token saved to session for subsequent calls - success
+            return True
+
+        # We failed to get an access token for some reason
+        # TODO Add some proper error handling here
+        return False
+
+    # If we already have an access token then there's no need to look again for one
+    return True
+
+def getStravaAccessToken(query):
+
+    if session.get(ACCESS_TOKEN_KEY, None) is None:
+        config = yaml.safe_load(open("config.yml"))
+        client_id = config['fitbit']['client_id']
+        client_secret = config['fitbit']['client_secret']
+
+        # Encode the client ID and secret for authentication when requesting a token
+        auth = str(base64.b64encode(bytes(f'{client_id}:{client_secret}', 'utf-8')), "utf-8")
+
+        # Get the code from the permission request response
+        code = getParameter(query, 'code')
+
+        # Get an authorisation token
+        endpoint = "https://api.fitbit.com/oauth2/token"
+        data = {'clientId': client_id, 'grant_type': 'authorization_code', 'redirect_uri': 'http://127.0.0.1:5000/fitbitauth',
                 'code': code}
         headers = {"Authorization": f"Basic {auth}",
                    'Content-Type': 'application/x-www-form-urlencoded'}
@@ -79,10 +148,32 @@ def getAccessToken(query):
     return True
 
 
-def auth(query):
-    if getAccessToken(query):
-        access_token = session.get(ACCESS_TOKEN_KEY, None)
+def fitbitAuth(query):
+    if getFitbitAccessToken(query):
+        return html.Div([
+            html.H3('Fitbit authenticated'),
+            dcc.Link('Dashboard', href='/dashboard')
+        ])
+    return html.Div([
+        html.H3('Fitbit authentication error'),
+        html.P('An error occurred')
+    ])
 
+def stravaAuth(query):
+    if getStravaAccessToken(query):
+        return html.Div([
+            html.H3('Strava authenticated'),
+            dcc.Link('Dashboard', href='/dashboard')
+        ])
+    return html.Div([
+        html.H3('Strava authentication error'),
+        html.P('An error occurred')
+    ])
+
+
+def dashboard():
+    access_token = session.get(ACCESS_TOKEN_KEY, None)
+    if access_token is not None:
         return dbc.Container(
             [
                 dbc.Row(
@@ -200,7 +291,7 @@ def getRestingHeartRateGraph(access_token):
     dates = list(map(lambda x: x['dateTime'], hr['activities-heart']))
 
     # TODO Handle things if there is no resting heart rate for the day (probably caused by not syncing yet?)
-    resting_hr = list(map(lambda x: x['value'].get('restingHeartRate', 0), hr['activities-heart']))
+    resting_hr = list(map(lambda x: x['value'].get('restingHeartRate', None), hr['activities-heart']))
 
     return dcc.Graph(
         id='resting-hr',
@@ -257,15 +348,15 @@ def getSleepHistoryGraph(access_token):
 
     dates = list(map(lambda x: x['dateOfSleep'], hr['sleep']))
 
-    deep = list(map(lambda x: x['levels']['summary'].get('deep', {'minutes': 0})['minutes'], hr['sleep']))
-    light = list(map(lambda x: x['levels']['summary'].get('light', {'minutes': 0})['minutes'], hr['sleep']))
-    rem = list(map(lambda x: x['levels']['summary'].get('rem', {'minutes': 0})['minutes'], hr['sleep']))
-    wake = list(map(lambda x: x['levels']['summary'].get('wake', {'minutes': 0})['minutes'], hr['sleep']))
+    deep = list(map(lambda x: x['levels']['summary'].get('deep', {'minutes': None})['minutes'], hr['sleep']))
+    light = list(map(lambda x: x['levels']['summary'].get('light', {'minutes': None})['minutes'], hr['sleep']))
+    rem = list(map(lambda x: x['levels']['summary'].get('rem', {'minutes': None})['minutes'], hr['sleep']))
+    wake = list(map(lambda x: x['levels']['summary'].get('wake', {'minutes': None})['minutes'], hr['sleep']))
 
     # Data from sleeps not long enough to be tracked with sleep stages
-    awake = list(map(lambda x: x['levels']['summary'].get('awake', {'minutes': 0})['minutes'], hr['sleep']))
-    asleep = list(map(lambda x: x['levels']['summary'].get('asleep', {'minutes': 0})['minutes'], hr['sleep']))
-    restless = list(map(lambda x: x['levels']['summary'].get('restless', {'minutes': 0})['minutes'], hr['sleep']))
+    awake = list(map(lambda x: x['levels']['summary'].get('awake', {'minutes': None})['minutes'], hr['sleep']))
+    asleep = list(map(lambda x: x['levels']['summary'].get('asleep', {'minutes': None})['minutes'], hr['sleep']))
+    restless = list(map(lambda x: x['levels']['summary'].get('restless', {'minutes': None})['minutes'], hr['sleep']))
 
     return dcc.Graph(
         id='sleep-history',
@@ -335,8 +426,12 @@ def getParameter(query, param):
 def display_page(pathname, search):
     if pathname == '/':
         return login()
-    elif pathname == '/auth':
-        return auth(search)
+    elif pathname == '/fitbitauth':
+        return fitbitAuth(search)
+    elif pathname == '/stravaauth':
+        return stravaAuth(search)
+    elif pathname == '/dashboard':
+        return dashboard()
     else:
         return html.H1('404')
 
